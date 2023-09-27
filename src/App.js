b8d85f14c03237de5e79bb7e4a565db6a4c80efa
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-27 10:05:45 +0200

    feat: add sorting layout

--- src/App.js
+++ src/App.js
@@ -90,6 +90,15 @@ function PackingList({ items, onToggleItem, onDeleteItem }) {
           />
         ))}
       </ul>
+
+      <div className='actions'>
+        <select>
+          <option value='input'>Sort by input order</option>
+          <option value='description'>Sort by description</option>
+          <option value='packed'>Sort by packed status</option>
+        </select>
+        <button>Clear list</button>
+      </div>
     </div>
   );
 }
