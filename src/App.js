078fdce04cb0f81fd7a33ae8e6ad8c56744f5acb
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-27 09:35:59 +0200

    feat: implement adding new item

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className='app'>
       <Logo />
-      <Form />
+      <Form onAddItems={handleAddItems} />
       <PackingList />
       <Stats />
     </div>
@@ -13,10 +21,42 @@ function Logo() {
   return <h1>Far Away</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { id: Date.now(), quantity, description, packed: false };
+    onAddItems(newItem);
+
+    setQuantity(1);
+    setDescription('');
+  }
+
   return (
-    <form class='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h2>What do you need for your trip?</h2>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <label htmlFor='item'>Write here:</label>
+      <input
+        type='text'
+        placeholder='Ex: passport'
+        name='item'
+        id='item'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
     </form>
   );
 }
