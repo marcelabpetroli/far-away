351780d3823147237a677ff3d9bad33933ff890f
Marcela Petroli <marcelabpetroli@gmail.com>
2023-09-27 09:23:45 +0200

    fetat: add components structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 function App() {
   return (
     <div className='app'>
-      <h1>Far Away</h1>
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
+function Logo() {
+  return <h1>Far Away</h1>;
+}
+
+function Form() {
+  return (
+    <form class='add-form'>
+      <h2>What do you need for your trip?</h2>
+    </form>
+  );
+}
+
+function PackingList() {
+  return (
+    <div className='list'>
+      <ul>
+        <Item />
+      </ul>
+    </div>
+  );
+}
+
+function Item() {
+  return <li></li>;
+}
+
+function Stats() {
+  return (
+    <footer className='stats'>
+      You've X items on your list. You've already packed X (X%).
+    </footer>
+  );
+}
 export default App;
